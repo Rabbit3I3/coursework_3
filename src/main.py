a10d4ddadc2b3c59cd4a45871762726578d48a59
egor <smithegor20@gamil.com>
2023-10-29 17:05:15 +0300

    прописал функцию

--- src/main.py
+++ src/main.py
@@ -1,1 +1,10 @@
-from utils import
+import utils
+
+filename = 'operations.json'
+
+load = utils.load_options(filename)
+five_last = utils.last_five_operations(load)
+print_operation = utils.prin_operations(five_last)
+
+for operation in print_operation:
+    print(operation)
